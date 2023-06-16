0bae241ffc34fe3c77004e2ed86c7ac4c8145c86
seo myeong jin <seomyeongjin99@gmail.com>
2023-06-16 15:06:19 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -2,13 +2,12 @@ function solution(price, money, count) {
     var answer = 0;
     var total_price=0;
     //price만큼 count번 증가한 총합을 구한다.
-    for(let i=0; i<= count; i++){
+    for(let i=1; i<= count; i++){
         total_price += price*i
     }
     answer = total_price - money
     if(answer<=0){
         return 0
     }
-    //총합 - 내가가진 돈
     return answer;
 }
