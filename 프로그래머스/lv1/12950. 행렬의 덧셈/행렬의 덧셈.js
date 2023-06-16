dc169156df75fd3472d9370198ea37b79198793c
seo myeong jin <seomyeongjin99@gmail.com>
2023-06-16 14:18:15 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.14 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,13 @@
 function solution(arr1, arr2) {
     let answer = [];
     
-    for(let i=0;i<arr1.length;i++){
-        answer.push([])
-        for(let j=0;j<arr2[0].length;j++){
-            answer[i][j] = arr1[i][j]+arr2[i][j]
-      }    
-    }
-    return answer;
+    // for(let i=0;i<arr1.length;i++){
+    //     answer.push([])
+    //     for(let j=0;j<arr2[0].length;j++){
+    //         answer[i][j] = arr1[i][j]+arr2[i][j]
+    //   }    
+    // }
+    // return answer;
+    
+    return arr1.map((arr1,idx1) => arr1.map((val,idx2) => val+arr2[idx1][idx2]));
 };
