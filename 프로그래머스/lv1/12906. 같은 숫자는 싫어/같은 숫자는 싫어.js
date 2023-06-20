72f9429ad7f7f6f4c69a8a3209c4b76d4cdfc166
seo myeong jin <seomyeongjin99@gmail.com>
2023-06-21 00:38:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.42 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,36 @@
 function solution(arr){
-    var answer = [];
-    for (let i = 0; i < arr.length; i++) {
+    let answer = [];
+    
+    for(let i=0; i<arr.length; i++) {
         if(arr[i] !== arr[i+1]) {
-        answer.push(arr[i])
+            answer.push(arr[i])
         }
     }
-    return answer;
+    return answer
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // var answer = [];
+    // for (let i = 0; i < arr.length; i++) {
+    //     if(arr[i] !== arr[i+1]) {
+    //     answer.push(arr[i])
+    //     }
+    // }
+    // return answer;
